3aadc58957317789b62d020271202588e788e969
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-02-16 12:44:02 -0600

    use split on first # to avoid issues for double #-es; related to #49

--- js/permalink.js
+++ js/permalink.js
@@ -1,8 +1,8 @@
 
 function permalink(urlString, speciesData) {
    //The current URL may or may not have a "#" at the end already. Handle both cases.
-   var baseUrl = urlString.charAt(urlString.length - 1) === "#" ? urlString : urlString + "#";
-   var permalink = baseUrl + "genus=" + speciesData.genus + "&" + "species=" + speciesData.specificEpithet + "&" + "id=" + speciesData.id;
+   var baseUrl = urlString.split("#")[0];
+   var permalink = baseUrl + "#genus=" + speciesData.genus + "&" + "species=" + speciesData.specificEpithet + "&" + "id=" + speciesData.id;
    return permalink;
 }
 
