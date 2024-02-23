facb16e8198d18d249c7cafa5f818d6c26877f00
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-02-23 17:24:26 -0600

    load maps when needed (aka lazy load) instead of all at once; related to https://github.com/mammaldiversity/mammaldiversity.github.io/issues/55

--- assets/update-countries.sh
+++ assets/update-countries.sh
@@ -8,8 +8,8 @@
 #
 #
 
-cat $(dirname $0)/countryCodes.csv\
+cat $(dirname $0)/../_data/countryCodes.csv\
  | cut --delimiter ',' -f1\
  | tail -n+2\
- | awk '{ print "-o countryShapes/" $1 ".json https://geodata.ucdavis.edu/gadm/gadm4.1/json/gadm41_" $1 "_0.json" }'\
+ | awk '{ print "-o countries/" $1 ".json https://geodata.ucdavis.edu/gadm/gadm4.1/json/gadm41_" $1 "_0.json" }'\
  | xargs -L1 curl
